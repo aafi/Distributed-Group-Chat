b25db022dd401f6d9fc717ef8cd698e6679f3e5b
aafi <anwesha@seas.upenn.edu>
2015-03-30 01:14:40 -0400

    Sequencer
    Updated. Not Completed.

--- source/sequencer.c
+++ source/sequencer.c
@@ -2,28 +2,53 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <stdio.h>
+#include <sys/queue.h>
 
 #define PORT 5678
 #define NPACK 10
-#define BUFLEN 512
+#define BUFLEN 1024
 #define MAX 15
 
-char * id[MAX] = {NULL};
+int id[MAX] = {0};
+int msg_seq_id = 1;
 
-int requestid(char * name)
+struct client{
+   char ip[BUFLEN];
+   int port;
+   int client_id;
+   int last_msg_id;     //id of the last message sent by the client
+  // int leader;          //by default is 0. The client which is the leader will have 1
+}client_list[MAX];
+
+struct message{
+   int client_id;
+   int msg_id;
+   char msg[BUFLEN];
+}
+
+
+int requestid(char * ip, int port)
 {
    int i;
    for(i=0;i<MAX;i++)
    {
-      if(id[i]==NULL)
+      if(id[i]==0)
       {  
-         strcpy(id[i],name);
+         struct client c;
+         strcpy(c.ip,ip);
+         c.port = port;
+         c.last_msg_id = 0;
+         c.client_id = i;
+        // c.leader = 0;
+         client_list[i] = c;
+         id[i] = 1;
          return i;
       }
    }
    return -1;                    //MAX limit reached; New participant cannot be added
 
 }
+
 
 int main(int argc, char *argv[]){
    struct sockaddr_in server, client;
@@ -61,25 +86,76 @@ int main(int argc, char *argv[]){
       if (strcmp("Request",token)==0)
       {
          int seq;
-         token = strtok(NULL, "#"); // Tokenizes received message to get name of the new client
-         seq = requestid(token);   // Gets back a sequence number for the new client
+         int i=0;
+         while(token !=NULL)
+         {
+            token = strtok(NULL,"#");
+            tok[i] = token;
+            i++;
+         }
+
+         seq = requestid(tok[0],atoi(tok[1]));   // Gets back a sequence number for the new client
+
          if(seq == -1)
             strcpy(reply,"Max Limit reached");
          else
             sprintf(reply, "%d", seq);
+
+         if((sendto(s,reply,sizeof(reply),0,(struct sockaddr*)&client, sizeof(client))) < 0)    //send reply back
+         {
+            perror("Send Error");
+            exit(-1);
+         }
+
+         char multi[BUFLEN] = "Seq";
+         char temp[BUFLEN];
+
+         int i = 0;
+         for(i;i<MAX;i++)
+         {
+            if(id[i]!=0)
+            {
+               strcat(multi,"#");
+               strcat(multi,client_list[i].ip);
+               strcat(multi,"#");
+               sprintf(temp,"%d",client_list[i].port);
+               strcat(multi,temp);
+               strcat(multi,"#");
+               sprintf(temp,"%d",client_list[i].client_id);
+               strcat(multi,temp);
+
+            }
+
+         }
+
       }
 
       else if (strcmp("Message",token)==0)
       {
-         
+         while(token!=NULL)
+         {  
+            int i = 0;
+            token = strtok(NULL,"#");
+            tok[i] = token;
+            i++;
+         }
 
+         int client_id,msg_id;
+         char msg[BUFLEN];
+
+         client_id = atoi(tok[0]);
+         msg_id = atoi(tok[1]);
+         strcpy(msg,tok[2]);
+
+         
       }
 
 
-      if((sendto(s,reply,sizeof(reply),0,(struct sockaddr*)&client, sizeof(client))) < 0)    //send reply back
+   //MULTICAST PART ----- DO IT
+      int k = 0;
+      for (k;k<MAX;k++)
       {
-         perror("Send Error");
-         exit(-1);
+
       }
 
    }
@@ -92,30 +168,3 @@ int main(int argc, char *argv[]){
 
 
 
-// int main(int argc, char *argv[])
-// {
-//    int sockfd,n;
-//    struct sockaddr_in servaddr,cliaddr;
-//    socklen_t len;
-//    char mesg[1000];
-
-//    sockfd=socket(AF_INET,SOCK_DGRAM,0);
-
-//    bzero(&servaddr,sizeof(servaddr));
-//    servaddr.sin_family = AF_INET;
-//    servaddr.sin_addr.s_addr=htonl(INADDR_ANY);
-//    servaddr.sin_port=htons(32000);
-//    bind(sockfd,(struct sockaddr *)&servaddr,sizeof(servaddr));
-
-//    while(1)
-//    {
-//       len = sizeof(cliaddr);
-//       n = recvfrom(sockfd,mesg,1000,0,(struct sockaddr *)&cliaddr,&len);
-//       sendto(sockfd,mesg,n,0,(struct sockaddr *)&cliaddr,sizeof(cliaddr));
-//       printf("-------------------------------------------------------\n");
-//       mesg[n] = 0;
-//       printf("Received the following:\n");
-//       printf("%s",mesg);
-//       printf("-------------------------------------------------------\n");
-//    }
-// }
