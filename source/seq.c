3455339a57cf401b1088ffa7a4c9569b452a163f
aafi <anwesha@seas.upenn.edu>
2015-04-11 16:03:41 -0400

    expects 2 pings every 10 secs

--- source/seq.c
+++ source/seq.c
@@ -637,7 +637,7 @@ void* message_pinging(int sock)
     for(item_client = TAILQ_FIRST(&client_head);item_client!=NULL;item_client=tmp_item)
     {
       tmp_item = TAILQ_NEXT(item_client,entries);
-      if(item_client->counter<10)
+      if(item_client->counter<2)
       {
         printf("less pings zomg!\n");
         char req_status[BUFLEN] = "STATUS";
