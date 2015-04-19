adefdded83e1e8484e52f11f5a780a1e6fe3474d
pdeepti <pdeepti@seas.upenn.edu>
2015-04-18 23:19:12 -0400

    Checked with printf. Now back to normal

--- source/dchat.c
+++ source/dchat.c
@@ -367,7 +367,7 @@ void* housekeeping(int soc){
 		if(recvfrom(soc, recvBuff, MAXSIZE, 0, (struct sockaddr*)&other_user_addr, &other_addr_size) < 0){
 			perror("Error: Receiving message failed \n");
 		} else {
-			// fprintf(stderr, "CLIENT: %s\n", recvBuff);
+			 //fprintf(stderr, "CLIENT RECEIVED: %s\n", recvBuff);
 		}
 
 		char* message[MAXSIZE], copy[MAXSIZE];
