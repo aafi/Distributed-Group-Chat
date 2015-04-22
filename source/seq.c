bdc0e546a12e725e72273716ba4fd0c42b9f777a
aafi <anwesha@seas.upenn.edu>
2015-04-21 23:17:46 -0400

    matched parathesis?

--- source/seq.c
+++ source/seq.c
@@ -818,12 +818,12 @@ void* message_multicasting(int s)
                   free(item);
                 }
               } // end of if (finding the right client structure)
-            }
+            //}
           }   // end of for (looping through id array to find the existing clients)
           // else
           //   printf("CLIENT QUEUE IS EMPTY\n");
 
-      //} // end of foreach (traversing through the message queue)
+      } // end of foreach (traversing through the message queue)
           // else
           //   printf("MESSAGE QUEUE IS EMPTY\n");
     }
